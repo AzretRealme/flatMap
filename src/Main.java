4d9a96588bb9a03d536c974370e7489975b15927
AzretRealme <71967119+AzretRealme@users.noreply.github.com>
2021-04-07 20:01:41 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
 
@@ -55,85 +51,5 @@ public class Main {
                 .collect(Collectors.toList());
 championsLigue.forEach(System.out::println);
 
-
-        //        List<String> namesOfCoach = Arrays.asList("Ole Gunnar", "Zidane", "Rudi Garcia");
-//.filter(club -> club.startsWith("A"))
-//        long count = namesOfCoach.stream()
-//                .filter(i -> i.length() > 4)
-//                .count();
-//        System.out.println(count);
-
-//        Gift gift1 = new Gift()
-//        List<String> gift12 = clubStream()
-//                .filter(club -> club.startsWith("Ri"))
-//                .collect(Collectors.toList());
-//map, flatmap, filter aufgabe
     }
 }
-
-//  Searchable wasFounded = club -> club.getWasFounded() >= 1950;
-//
-//        Searchable club1 = club -> {
-//            switch (club.getCountry()){
-//                case FRANCE: return true;
-//                case ENGLAND:
-//                case GERMANY:
-//                case SPAIN: return false;
-//
-//            }
-//            return false;
-//        };
-//        System.out.println("--------Search Club 1950");
-//        clubs.forEach(club -> System.out.println(club1.search(club)));
-//
-//        Searchable ligue = club -> {
-//            switch (club.getLigue()){
-//                case PremierLigue:
-//                case LaLiga:
-//                case BundesLiga:
-//                case SeriaA: return false;
-//                case Ligue1: return true;
-//            }
-//            return false;
-//        };
-//        System.out.println("--------Search Club 1950 from Ligue");
-//        clubs.forEach(club -> System.out.println(ligue.search(club)));
-//        System.out.println("--------Search was founded 1950");
-//        clubs.forEach(club -> System.out.println(wasFounded.search(club)));
-
-        //clubs.forEach(club -> System.out.println(ligue.search(club) && club1.search(club) && wasFounded.search(club)));
-
-//    }
-//
-//}
-//        Stream<Club> clubStream = clubs.stream();
-//        Stream.of(new Club("ManchesterUnited", 1878, Country.ENGLAND, Ligue.PremierLigue),
-//                new Club( "RealMadrid", 1902, Country.SPAIN, Ligue.LaLiga),
-//                new Club("BayernMunich", 1900, Country.GERMANY, Ligue.BundesLiga));
-//        //clubs.forEach(number -> System.out.println(clubs));
-//        Club[] clubArray = {new Club("BayernMunich", 1900, Country.GERMANY, Ligue.BundesLiga),
-//                new Club("JUVENTUS", 1897, Country.ITALY, Ligue.SeriaA),
-//                new Club("Lion", 1950, Country.FRANCE, Ligue.Ligue1)};
-//        Stream<Club> clubStreamArray = Arrays.stream(clubArray);
-//        String clubName = "RealMadriddd";
-//        IntStream clubsIntstream = clubName.chars();
-//        Stream<Object> clubStreamBuilder = Stream
-//                .builder()
-//                .add(new Club("Dortmund", 1901, Country.GERMANY, Ligue.BundesLiga))
-//                .build();
-////        long count = clubStream
-////                .filter(club -> club.getWasFounded() == 1950)
-////                .count();
-////        System.out.println(count);
-//        List<Club> filteredClubs = clubStream
-//                .filter(club -> club.getWasFounded() == 1950 && club.getCountry() == Country.ENGLAND)
-//                .collect(Collectors.toList());
-//        System.out.println(filteredClubs);
-//        System.out.println(clubs.stream()
-//        .filter(club -> club.getWasFounded() == 1950 && club.getCountry() == Country.ENGLAND)
-//    }
-//}
-//@FunctionalInterface
-//interface Searchable{
-//    boolean search(Club club);
-//}
